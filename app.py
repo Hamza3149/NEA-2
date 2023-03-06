0ca95604352e6c96ea349be15912050cb1461b54
th0mas-w <thomaswesthead@icloud.com>
2023-03-06 20:58:36 +0000

    update

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from functools import wraps
 import sqlite3
 
 
-
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.secret_key = 'secret_key'
-#db = SQLAlchemy(app)
-
 
 app.config["SESSION_PERMENANT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -20,27 +13,17 @@ Session(app)
 conn = sqlite3.connect("data.db", check_same_thread=False)
 cursor = conn.cursor()
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.after_request
 def after_request(response):
-    """Ensure responses aren't cached"""
+    """Ensure responses aren"t cached"""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
-
-"""
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-"""
 
 def login_required(f):
     @wraps(f)
@@ -50,14 +33,14 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-@app.route('/home')
+@app.route("/home")
 @login_required
 def home():
     cursor.execute("SELECT username FROM users")
     users = cursor.fetchall()
     print("users:",users)
     
-    return render_template('home.html',username=session["username"] , users=users)
+    return render_template("home.html",username=session["username"] , users=users)
 
 
 @app.route("/logout")
@@ -66,43 +49,39 @@ def logout():
     session.clear()
     return redirect("/")
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     session.clear()
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        cursor.execute("SELECT * FROM users WHERE username = ?",(username, ))
+        existing_users = cursor.fetchall()
+
+        if len(existing_users) > 0:
+            flash("That username already exists")
         
         cursor.execute("INSERT INTO users (username, password) VALUES (?,?)",(username, password))
         conn.commit()
 
-        return redirect(url_for('index'))
-    return render_template('register.html')
+        session["username"] = username
 
-@app.route('/login', methods=['GET', 'POST'])
+        return redirect("/home")
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     session.clear()
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         cursor.execute("SELECT * FROM users WHERE username = ?",(username, ))
         user = cursor.fetchall()
-        if len(user) > 0:
-            if user[0][2] == password:
-                session["username"] = request.form["username"]
-                return redirect(url_for('home'))
+        if len(user) > 0 and user[0][2] == password:
+            session["username"] = request.form["username"]
+            return redirect("/home")
 
-        flash('Incorrect username or password!')
-    return render_template('login.html')
+        flash("Incorrect username or password!")
+    return render_template("login.html")
 
-"""
-@app.before_first_request
-def create_tables():
-    db.create_all()
-"""
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
