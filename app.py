4f52597574617e0cea367fb423ac5bc5fc223453
th0mas-w <thomaswesthead@icloud.com>
2023-03-06 16:12:23 +0000

    update

--- app.py
+++ app.py
@@ -7,15 +7,18 @@ import sqlite3
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'secret_key'
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.secret_key = 'secret_key'
+#db = SQLAlchemy(app)
 
 
 app.config["SESSION_PERMENANT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+conn = sqlite3.connect("data.db", check_same_thread=False)
+cursor = conn.cursor()
 
 @app.route('/')
 def index():
@@ -29,6 +32,7 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+"""
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -36,6 +40,7 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+"""
 
 def login_required(f):
     @wraps(f)
@@ -48,15 +53,11 @@ def login_required(f):
 @app.route('/home')
 @login_required
 def home():
-    if "username" in session:
-        return render_template("home.html",username=session["username"])
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT username FROM user')
+    cursor.execute("SELECT username FROM users")
     users = cursor.fetchall()
-    conn.close()
-    users = [('1', 'Alice'), ('2', 'Bob'), ('3', 'Charlie')]
-    return render_template('home.html', users=users)
+    print("users:",users)
+    
+    return render_template('home.html',username=session["username"] , users=users)
 
 
 @app.route("/logout")
@@ -71,9 +72,10 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = user(username=username, password=password)
-        db.session.add(user)
-        db.session.commit()
+        
+        cursor.execute("INSERT INTO users (username, password) VALUES (?,?)",(username, password))
+        conn.commit()
+
         return redirect(url_for('index'))
     return render_template('register.html')
 
@@ -83,18 +85,21 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username, password=password).first()
-        if user:
-            session["username"] = request.form["username"]
-            return redirect(url_for('home'))
-        else:
-            flash('Incorrect username or password!')
+        cursor.execute("SELECT * FROM users WHERE username = ?",(username, ))
+        user = cursor.fetchall()
+        if len(user) > 0:
+            if user[0][2] == password:
+                session["username"] = request.form["username"]
+                return redirect(url_for('home'))
+
+        flash('Incorrect username or password!')
     return render_template('login.html')
 
+"""
 @app.before_first_request
 def create_tables():
     db.create_all()
-
+"""
 
 
 
